406d423d422baad544e47729d75a647b1779210a
SaiMahith Chigurupati <chigurupati.sa@northeastern.edu>
2023-01-29 13:08:14 -0500

    postgres setup completed

--- api/models/index.js
+++ api/models/index.js
@@ -1,3 +1,45 @@
 import Users from './users.js';
 
+import dbConfig from '../config/dbConfig.js';
+import {Sequelize, DataTypes} from 'sequelize';
+
+const sequelize = new sequelize(
+    dbConfig.DB,
+    dbConfig.USER,
+    dbConfig.PASSWORD,{
+        host:dbConfig.HOST,
+        dialect: dbConfig.dialect,
+        operationAliases: false,
+
+        pool: {
+            max: dbConfig.pool.max,
+            min: dbConfig.pool.acquire.min,
+            acquire: dbConfig.pool.acquire,
+            idle: dbConfig.pool.idle
+        }
+    }
+)
+
+sequelize.authenticate()
+.then(()=>{
+    console.log('connected..')
+})
+.catch(err=>{
+    console.log('Error:'+err);
+})
+
+const db = {}
+
+db.Sequelize = Sequelize
+db.sequelize = sequelize
+
+db.users = require('./users')(sequelize,DataTypes  )
+
+db.sequelize.sync({force : false})
+.them(()=>{
+    console.log('resync done..')
+})
+
+modules.export = db
+
 export default { Users };
