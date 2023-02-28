be27e8b4580af014e01b57c3e118b3068dd97cdb
SaiMahith Chigurupati <chigurupati.sa@northeastern.edu>
2023-02-28 03:49:36 -0500

    bug fix

--- script.sh
+++ script.sh
@@ -8,11 +8,29 @@ sudo yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 sudo yum install -y nodejs
 
+pwd
+
 unzip webapp.zip
+
+pwd
+
+ls
+
 cd webapp
+
+pwd
+
+ls
+
 npm i --save
 
+ls
+
 touch app.env
+
+pwd
+
+ls
 
 cat >> webapp.service <<'EOF'
 [Unit]
@@ -20,17 +38,21 @@ Description=webapp
 After=multi-user.target
 
 [Service]
-ExecStart=/usr/bin/node /home/ec2-user/webapp/server.js
+ExecStart=/usr/bin/node /home/ec2-user/webapp-main/server.js
 Restart=always
 RestartSec=10
 StandardOutput=syslog
 StandardError=syslog
 SyslogIdentifier=webapp
 User=ec2-user
-EnvironmentFile=/home/ec2-user/webapp/app.env
+EnvironmentFile=/home/ec2-user/app.env
 
 [Install]
 WantedBy=multi-user.target
 EOF
+
+pwd
+
+ls
 
 sudo cp -R "/home/ec2-user/webapp.service" "/etc/systemd/system/"
